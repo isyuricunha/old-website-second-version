bc37b2addf3da7009e40246ace5cebc5c2ba4dcc
Yuri Cunha <me@yuricunha.com>
2024-04-24 19:58:37 -0300

    Update next.config.js with webpack and output options

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  server: {
-    port: 8000,
-    host: "0.0.0.0",
-  },
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        "fs": false,
-        "path": false,
-        "os": false,
-      }
-    }
-    return config
-  },
-
+	reactStrictMode: true,
+	server: {
+		port: 8000,
+		host: "0.0.0.0",
+	},
+	webpack: (config) => {
+		config.resolve = {
+			...config.resolve,
+			fallback: {
+				fs: false,
+				path: false,
+				os: false,
+			},
+		};
+		return config;
+	},
+	// Adicione a opção output para exportação estática
+	output: "export",
 };
 
 module.exports = nextConfig;
