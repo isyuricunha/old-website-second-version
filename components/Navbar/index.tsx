a062841abc8be5c56e0e4070a4d791f1bc9565c3
Yuri Cunha <me@yuricunha.com>
2024-04-24 19:41:21 -0300

    fix <link> with <a> tag to can run project

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -6,17 +6,14 @@ export const Navbar: FC = () => {
 		<div className="flex items-center justify-center sticky p-5 top-0 backdrop-blur-lg">
 			<div className="flex md:w-7/12 w-10/12 justify-between">
 				<div className="flex items-center">
-					<Link href="/">
+					<Link href="/" legacyBehavior>
 						<a className="text-2xl font-bold text-gray-200">yuricunha.</a>
 					</Link>
 				</div>
 				<div className="flex items-center text-gray-300">
-					<Link href="/projects">
+					<Link href="/projects" className="mx-3" legacyBehavior>
 						<a className="mx-3">Projects</a>
 					</Link>
-					{/* <a href="www.yuricunha.com" className="mr-3">
-            Blog
-          </a> */}
 					<a
 						href="mailto:isyuricunha@duck.com"
 						className="px-5 p-2 transition rounded-full hover:bg-[#262626] bg-[#1f1f1f]"
